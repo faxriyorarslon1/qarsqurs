0b5382c29db7494d87cb4c58a1125a7e4ce4bacc
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2022-08-14 22:56:40 +0500

    add static root, remove staticfiles dirs

--- qarsqurs/settings.py
+++ qarsqurs/settings.py
@@ -130,14 +130,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+import os 
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = 'media/'
-MEDIAFILES_DIRS = [
-    BASE_DIR / "media",
-]
+# MEDIAFILES_DIRS = [
+#     BASE_DIR / "media",
+# ]
 
 
 # Default primary key field type
