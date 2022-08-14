90fc5c56be3af195174ec637921905b94f52a6ac
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2022-08-14 22:57:24 +0500

    add static root, remove staticfiles dirs2

--- qarsqurs/settings.py
+++ qarsqurs/settings.py
@@ -137,7 +137,7 @@ import os
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = 'media/'
 # MEDIAFILES_DIRS = [
