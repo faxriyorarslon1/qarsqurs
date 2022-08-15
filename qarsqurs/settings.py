ea76b610b6f61af83070c5a692f046f8b358e003
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2022-08-15 19:24:31 +0500

    change static root

--- qarsqurs/settings.py
+++ qarsqurs/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,22 +128,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+#     )
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-import os 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'assets/')
-
-# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-MEDIA_URL = 'media/'
-MEDIAFILES_DIRS = [
-    BASE_DIR / "media",
-]
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIAFILES_DIRS = (
+    os.path.join(BASE_DIR,'media'),
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
