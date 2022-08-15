a3dc37f87247147f306f1a1a75e5609f2617e223
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2022-08-15 19:32:12 +0500

    change static roo5t

--- qarsqurs/settings.py
+++ qarsqurs/settings.py
@@ -129,14 +129,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR,'static'),
-#     )
+STATIC_ROOT = "/var/www/course.fakhriyor.uz/static/"
+MEDIA_ROOT = "/var/www/course.fakhriyor.uz/media/"
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+    )
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIAFILES_DIRS = (
     os.path.join(BASE_DIR,'media'),
     )
